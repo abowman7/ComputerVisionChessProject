f13d78f06f71fab82edf96d45828619fecff8694
Skytlz <skytlzdev@gmail.com>
2024-12-04 13:51:34 -0500

    Remove Useless comments

--- PieceWeights.py
+++ PieceWeights.py
@@ -17,68 +17,6 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint
 
-# classifications = [
-#     "white_pawn_white_space", #1
-#     "white_pawn_black_space", #2
-#     "white_rook_white_space", #3
-#     "white_rook_black_space", #4
-#     "white_knight_white_space", #5
-#     "white_knight_black_space", #6
-#     "white_bishop_white_space", #7
-#     "white_bishop_black_space", #8
-#     "white_queen_white_space", #9
-#     "white_queen_black_space", #10
-#     "white_king_white_space", #11
-#     "white_king_black_space", #12
-    
-#     "black_pawn_white_space", #13
-#     "black_pawn_black_space", #14
-#     "black_rook_white_space", #15
-#     "black_rook_black_space", #15
-#     "black_knight_white_space", #16
-#     "black_knight_black_space", #17
-#     "black_bishop_white_space", #18
-#     "black_bishop_black_space", #20
-#     "black_queen_white_space", #21
-#     "black_queen_black_space", #22
-#     "black_king_white_space", #23
-#     "black_king_black_space", #24
-
-#     "empty_white_space", #25
-#     "empty_black_space", #26
-#     ]
-
-# num_classes = len(classifications)
-
-# classes = {
-#     "white_pawn_white_space": 0,
-#     "white_pawn_black_space": 1,
-#     "white_rook_white_space": 2,
-#     "white_rook_black_space": 3,
-#     "white_knight_white_space": 4,
-#     "white_knight_black_space": 5,
-#     "white_bishop_white_space": 6,
-#     "white_bishop_black_space": 7,
-#     "white_queen_white_space": 8,
-#     "white_queen_black_space": 9,
-#     "white_king_white_space": 10,
-#     "white_king_black_space": 11,
-#     "black_pawn_white_space": 12,
-#     "black_pawn_black_space": 13,
-#     "black_rook_white_space": 14,
-#     "black_rook_black_space": 15,
-#     "black_knight_white_space": 16,
-#     "black_knight_black_space": 17,
-#     "black_bishop_white_space": 18,
-#     "black_bishop_black_space": 19,
-#     "black_queen_white_space": 20,
-#     "black_queen_black_space": 21,
-#     "black_king_white_space": 22,
-#     "black_king_black_space": 23,
-#     "empty_white_space": 24,
-#     "empty_black_space": 25,
-# }
-
 classifications = [
     "BlackBishops", #1
     "BlackKings", #2
